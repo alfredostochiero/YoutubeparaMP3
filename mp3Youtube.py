cf20f0c68dc10c9870c7f45b54640d306c25ae23
unknown <alfredo_stochiero@hotmail.com>
2020-10-19 21:20:13 -0300

    Download working fine

--- mp3Youtube.py
+++ mp3Youtube.py
@@ -8,25 +8,37 @@ window = Tk()
 window.title("Converter Youtube em mp3")
 window.geometry('500x300')
 
-btn = Button(window, text="Clique aqui")
+lbl = Label(window, text="Cole a url no campo a seguir: ")
+lbl.grid(column=0, row=0)
 
-btn.grid(column=0, row=0)
+txt = Entry(window, width=35)
+txt.grid(column=1,row=0)
+
+
+
+def clicked():
+
+    diretorio = os.path.dirname(os.path.abspath(__file__))
+    
+    url = txt.get()
+    
+
+    video = pafy.new(url) 
+    title = video.title
+
+    lbl.configure(text=title+' baixado com sucesso !! ')
+
+    bestaudio = video.getbestaudio() 
+    bestaudio.download(filepath=diretorio+"/Musicas") 
+
+
+
+
+
+
+btn = Button(window, text="Clique aqui",command=clicked)
+
+btn.grid(column=2, row=0)
 
 window.mainloop()
 
-
-
-'''
-diretorio = os.path.dirname(os.path.abspath(__file__))
-  
-url = input("Cole o link da música : ")
-video = pafy.new(url) 
-titulo = video.title
-
-print(titulo)
-  
-bestaudio = video.getbestaudio() 
-bestaudio.download(filepath=diretorio+"/Musicas") 
-
-####
-'''
